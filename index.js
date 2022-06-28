330bb6a60a886fc467930da6413ec3f062196bc5
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-28 12:52:03 +0300

    commit tree 232d8008c0d3508da394edf78ef3fd5487051392

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius = radius;
+  }
+  get getDiameter(){
+    return this.radius * 2
+  }
+  get getCircumference(){
+     return 2 * Math.PI * this.radius
+  }
+  get getArea(){
+     return Math.PI * this.radius *this.radius
+  }
+}
